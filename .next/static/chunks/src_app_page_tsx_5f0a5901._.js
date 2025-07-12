8c9bff783acd0e5e048edddac12b3a551aea9b6b
Debadyuti Dey <debadyutidey7@gmail.com>
2025-07-12 17:22:20 +0530

    ...

---
+++ .next/static/chunks/src_app_page_tsx_5f0a5901._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_fb8e1340._.js",
+  "static/chunks/node_modules_pdfjs-dist_build_pdf_mjs_b3b2e714._.js",
+  "static/chunks/node_modules_04fc6771._.js"
+],
+    source: "dynamic"
+});
